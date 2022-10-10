e84ef6d2557ffd28e0129e42d163537065484007
Sapanay <115458418+Sapanay@users.noreply.github.com>
2022-10-10 15:11:08 +0530

    Create loop_factorial.c

---
+++ loop_factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j=1,n;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	j=j*i;
+	printf("the facrorial is:%d",j);
+	
+	
+}
