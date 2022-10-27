5ed0b9d254b3d21e0af4d5c3b49cae2791fa668b
punithbajaj <punith.bajaj.cd.ece19@itbhu.ac.in>
2022-10-28 03:59:51 +0530

    added c++ template for competative programming

---
+++ C++/template_for_cp.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define N 600
+#define forn(i,n) for(int i=0; i<n; i++)
+#define f first
+#define s second
+
+void solve()
+{
+    cout<<"Completed";
+}
+
+int main()
+{
+    int t = 1;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
